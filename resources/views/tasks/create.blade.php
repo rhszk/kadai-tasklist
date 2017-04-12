563a41bac0f2a09af0d25169378919b85346ad4e
rihasuzuki <riha117@icloud.com>
2017-04-12 09:07:07 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -8,9 +8,16 @@
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
-
+		<br>
+        {!! Form::label('status', 'ステータス:') !!}
+        高{!! Form::radio('status', '高') !!}
+        中{!! Form::radio('status', '中', true) !!}
+        低{!! Form::radio('status', '低') !!}
+		<br>
         {!! Form::submit('投稿') !!}
 
     {!! Form::close() !!}
+
+	{!! link_to_route('tasks.index', '一覧へ') !!}
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -8,9 +8,16 @@
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
-
+        <br>
+        {!! Form::label('status', 'ステータス:') !!}
+        高{!! Form::radio('status', '高') !!}
+        中{!! Form::radio('status', '中') !!}
+        低{!! Form::radio('status', '低') !!}
+        <br>
         {!! Form::submit('更新') !!}
 
     {!! Form::close() !!}
+
+ 	{!! link_to_route('tasks.index', '一覧へ') !!}
 
 @endsection
